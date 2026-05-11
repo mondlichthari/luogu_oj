a8e3487464502c315361903ab856ce2b4c64ef3a
Mingrui  Zhang <mrzmr999@163.com>
2026-05-11 16:57:25 +0800

    Update setprecision_case.cpp

--- setprecision_case.cpp
+++ setprecision_case.cpp
@@ -6,7 +6,7 @@ int main()
     int n;
     double t;
     cin>>t>>n;
-    cout<<fixed<<setprecision(3)<<t/n<<endl;//remember this format
+    cout<<fixed<<setprecision(3)<<t/n<<endl;//remember this format" fixed + setpricision()"
     cout<<2*n;
     return 0;
 }
