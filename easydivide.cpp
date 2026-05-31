87ba44036640a8e040f7e081199570be690177d2
minako J. <mrzmr999@163.com>
2026-05-31 11:12:41 +0800

    Add easydivide.cpp to calculate pen purchase limit
    Implement a program to calculate the maximum number of pens that can be bought with given money.

---
+++ easydivide.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,m,n;
+    cin>>a>>b;
+    m=10*a+b;
+    n=m/19;
+    cout<<n;
+    return 0;
+}
+//班主任给小玉一个任务，到文具店里买尽量多的签字笔。已知一只签字笔的价格是 1 元 9 角，而班主任给小玉的钱是 a 元 b 角，小玉想知道，她最多能买多少只签字笔呢。
